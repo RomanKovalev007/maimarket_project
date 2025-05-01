b409dcf5c29049bbec8cbf8620d6590938d92302
roman <romakov640@gmail.com>
2025-05-01 09:57:51 +0300

    первый коммит

---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.urls import reverse
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name='Email')
+    photo = models.ImageField(upload_to="users/%Y/%m/%d/", blank=True, null=True, verbose_name="Фотография")
+    number = models.PositiveBigIntegerField(blank=True, null=True, verbose_name='Контактный телефон')
+
+    def get_absolute_url(self):
+        return reverse('users:profile', kwargs={'user_id': self.id})
