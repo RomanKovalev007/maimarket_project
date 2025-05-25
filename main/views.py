2083da478ba35812cde2c1337ce3358be7c058e3
roman <romakov640@gmail.com>
2025-05-25 19:22:35 +0300

    немного поправил избранное

--- main/views.py
+++ main/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+from favorites.models import Favorites
 from goods.models import Goods
 
 # функция представления для отображения главной страницы
 def main_page(request):
     ads = Goods.objects.filter(is_published=1)
+    if request.user.is_authenticated:
+        for ad in ads:
+            if Favorites.objects.filter(user=request.user, product=ad).exists():
+                ad.is_favorite = 'is-active'
+            else:
+                ad.is_favorite = ''
     data = {'ads': ads, 'title': 'Mai Market'}
     return render(request, 'main/index.html', data)
 
